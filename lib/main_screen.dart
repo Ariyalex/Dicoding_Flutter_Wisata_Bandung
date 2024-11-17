9767130c58509907e02930f23d858efd2ff93eba
Ariyalex <ariya17duta@gmail.com>
2024-11-17 21:14:28 +0700

    feat: implement responsive layout for MainScreen with list and grid views

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -7,18 +7,37 @@ var infTxtStyle= const TextStyle(fontFamily: 'Futura', color: Colors.black, deco
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Wisata Bandung'),
+        title: Text(
+          'Wisata Bandung. Size: ${MediaQuery.of(context).size.width}',
+          style: infTxtStyle,
+        ),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final TourismPlace place = tourismPlaceList[index];
-                  return InkWell(
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            return const TourismPlaceList();
+          } else {
+            return const TourismPlaceGrid();
+          }
+        },
+      )
+    );
+  }
+}
+
+class TourismPlaceList extends StatelessWidget {
+  const TourismPlaceList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final TourismPlace place = tourismPlaceList[index];
+        return InkWell(
           onTap: () {
             Navigator.push(
               context,
@@ -47,10 +66,8 @@ class MainScreen extends StatelessWidget {
               children: <Widget>[
                 Expanded(
                   flex: 2,
-                  child: Image.asset(
-                      place.imageAsset
-                      ),
-                  ),
+                  child: Image.asset(place.imageAsset),
+                ),
                 Expanded(
                   flex: 3,
                   child: Padding(
@@ -60,9 +77,9 @@ class MainScreen extends StatelessWidget {
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
                         Text(
-                            place.name,
-                            style: infTxtStyle.copyWith(fontSize: 16.0, fontWeight: FontWeight.bold),
-                          ),
+                          place.name,
+                          style: infTxtStyle.copyWith(fontSize: 16.0, fontWeight: FontWeight.bold),
+                        ),
                         const SizedBox(
                           height: 10,
                         ),
@@ -75,8 +92,54 @@ class MainScreen extends StatelessWidget {
             ),
           ),
         );
-        },
-        itemCount: tourismPlaceList.length,
+      },
+      itemCount: tourismPlaceList.length,
+    );
+  }
+}
+
+class TourismPlaceGrid extends StatelessWidget {
+  const TourismPlaceGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: GridView.count(
+        crossAxisCount: 4,
+        children: [
+            Card(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.zero,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+              Expanded(
+                child: Image.asset(
+                tourismPlaceList[0].imageAsset,
+                fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                tourismPlaceList[0].name,
+                style: infTxtStyle.copyWith(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                child: Text(
+                tourismPlaceList[0].location,
+                style: infTxtStyle,
+                ),
+              ),
+              ],
+            ),
+            ),
+        ],
       ),
     );
   }
