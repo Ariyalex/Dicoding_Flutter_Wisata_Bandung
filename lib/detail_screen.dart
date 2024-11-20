8aac526e291a137bc18561f01052566fc28db9a0
Ariyalex <ariya17duta@gmail.com>
2024-11-20 17:41:34 +0700

    feat: implement responsive DetailScreen with mobile and web layouts, add favorite button functionality

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -5,7 +5,50 @@ var infTxtStyle= const TextStyle(fontFamily: 'Futura', color: Colors.black, deco
 
 class DetailScreen extends StatelessWidget {
   final TourismPlace place;
+
   const DetailScreen({super.key, required this.place});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constrains) {
+        if (constrains.maxWidth > 840) {
+          return DetailWebPage(place: place);
+        } else {
+          return DetailMobilePage(place: place);
+        }
+      },
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({super.key});
+
+  @override
+  FavoriteButtonState createState() => FavoriteButtonState();
+}
+
+class FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border, color: Colors.red,),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
+    );
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  final TourismPlace place;
+
+  const DetailMobilePage({super.key, required this.place});
 
   @override
   Widget build(BuildContext context) {
@@ -105,25 +148,153 @@ class DetailScreen extends StatelessWidget {
   }
 }
 
-class FavoriteButton extends StatefulWidget {
-  const FavoriteButton({super.key});
+class DetailWebPage extends StatefulWidget {
+  final TourismPlace place;
+
+  const DetailWebPage({super.key, required this.place});
 
   @override
-  FavoriteButtonState createState() => FavoriteButtonState();
+  State<DetailWebPage> createState() => _DetailWebPageState();
 }
 
-class FavoriteButtonState extends State<FavoriteButton> {
-  bool isFavorite = false;
+class _DetailWebPageState extends State<DetailWebPage> {
+  final _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border, color: Colors.red,),
-      onPressed: () {
-        setState(() {
-          isFavorite = !isFavorite;
-        });
-      },
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30.0,
+          vertical: 16.0,
+        ),
+        child: Center(
+          child: SizedBox(
+            width: 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Wisata Bandung',
+                  style: TextStyle(
+                    fontFamily: 'Futura',
+                    fontSize: 32.0,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(widget.place.imageAsset),
+                          ),
+                          const SizedBox(height: 16),
+                          Scrollbar(
+                            controller: _scrollController,
+                            child: Container(
+                              height: 150,
+                              padding: const EdgeInsets.only(bottom: 16),
+                              child: ListView(
+                                controller: _scrollController,
+                                scrollDirection: Axis.horizontal,
+                                children: widget.place.imageUrls.map((url) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(4.0),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Image.network(url),
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 32),
+                    Expanded(
+                      child: Card(
+                        child: Container(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Text(
+                                widget.place.name,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 30.0,
+                                  fontFamily: 'Futura',
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Row(
+                                    children: <Widget>[
+                                      const Icon(Icons.calendar_today),
+                                      const SizedBox(width: 8.0),
+                                      Text(
+                                        widget.place.openDays,
+                                        style: infTxtStyle,
+                                      )
+                                    ],
+                                  ),
+                                  const FavoriteButton(),
+                                ],
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  const Icon(Icons.access_time_rounded),
+                                  const SizedBox(width: 8.0),
+                                  Text(
+                                    widget.place.openTime,
+                                    style: infTxtStyle,
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: 8.0),
+                              Row(
+                                children: <Widget>[
+                                  const Icon(Icons.monetization_on),
+                                  const SizedBox(width: 8.0),
+                                  Text(
+                                    widget.place.ticketPrice,
+                                    style: infTxtStyle,
+                                  )
+                                ],
+                              ),
+                              Container(
+                                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                                child: Text(
+                                  widget.place.description,
+                                  textAlign: TextAlign.justify,
+                                  style: const TextStyle(fontSize: 16.0, fontFamily: 'Futura'),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
